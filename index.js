c60b073d4f552ecaa2c7b3eab922beeb2807ba4d
kayoTRZN <kayowilson219@gmail.com>
2024-04-17 13:49:28 -0300

    consumindo api usando fetch

---
+++ index.js
@@ -0,0 +1,13 @@
+async function getAddresByCep() {
+    const cep = document.getElementById('cep').value;
+    try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = await response.json();
+      console.log(data);
+      document.getElementById('rua').value = data.logradouro;
+      document.getElementById('bairro').value = data.bairro;
+      document.getElementById('cidade').value = data.localidade;
+    }catch (error) {
+      alert(error.message);
+    }
+}
